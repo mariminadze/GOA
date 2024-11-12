6c2be13a6025ab7d0a4e9066c7b4fe3fc92760bd
mariminadze <minadzemari64@gmail.com>
2024-11-12 15:44:47 +0400

    first commit

---
+++ level 02/homework/homework.py
@@ -0,0 +1,11 @@
+name = "მარიამი"
+surname = "მინაძე"
+age = 13
+hobby = "ცეკვა"
+country = "საქართველო"
+
+print(name)
+print(surname)
+print(age)
+print(hobby)
+print(country)
---
+++ level 05/homework/homework.py
@@ -0,0 +1,26 @@
+# current_year = 2024
+# print("The current year is " + current_year)
+
+# 2
+# country = "USA"
+# print("You are from " + Country)
+
+# 3
+# height = 180
+# print("Your height in cm is " + height / 100 + " meters")
+
+# 4
+# print("Hello, " + new_name)
+# new_name = "Alex"
+
+current_year = 2024
+print("The current year is " + str(current_year))
+
+country = "USA"
+print("You are from " + country)
+
+height = 180
+print("Your height in cm is " + str(height / 100) + " meters")
+
+new_name = "Alex"
+print("Hello, " + new_name)

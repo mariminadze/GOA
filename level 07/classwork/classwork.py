206496bab82c92976514765f5fcb30eefc227994
mariminadze <minadzemari64@gmail.com>
2024-11-23 22:40:12 +0400

    level 07

---
+++ level 07/classwork/classwork.py
@@ -0,0 +1,11 @@
+# making calculator (idk how to write calculator correctly)
+name=input("მარიამი მინაძე")
+password=input("t0tallynot.0.Mari")
+print(name,password)
+
+
+
+
+ 
+print("C A L C U L A T O R")
+num1 = int(input("77"))
---
+++ level 07/homework/homework.py
@@ -0,0 +1,21 @@
+# my kind of private information
+name=input("მარიამი")
+surname=input("მინაძე")
+age=str(input("13"))
+print(name+" "+surname+" "+str(age))
+
+# C A L C U L A T O R
+num1 = 64
+num2 = 36
+print(num1+num2)
+print(num1-num2)
+print(num1*num2)
+print(num1/num2)
+print(num1//num2)
+print(num1**num2)
+
+# my REALLY private information
+gmail=input("minadzemari64@gmail.com")
+password=input("t0tallynot.0.Mari")
+print(gmail+" "+password)
+
